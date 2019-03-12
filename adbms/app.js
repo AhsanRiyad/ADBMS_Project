81ee91f29f76144dfeb28dde4f1d8eb09f3ed245
AhsanRiyad <riyad298@gmail.com>
2019-03-12 09:39:13 +0600

    stable adbms

--- adbms/app.js
+++ adbms/app.js
@@ -1,7 +1,7 @@
 // declaration
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var auth = require.main.require('./controller/auth');
 var app = express();
 var port = 3000;
 
@@ -13,6 +13,21 @@ app.set('view engine' , 'ejs');
 
 // middlewares
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/auth' , auth);
+
+
+
+// routes
+app.get('/' , (req,res)=>{
+
+
+	res.render('login');
+
+
+	
+
+}
+);
 
 
 app.listen(port, ()=>console.log('server started at port '+port));
