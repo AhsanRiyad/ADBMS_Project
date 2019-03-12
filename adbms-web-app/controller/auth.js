07f863bf6139e5c1c931ce0d9e5cbfe167848a1c
unknown <riyad298@gmail.com>
2019-03-12 07:09:01 +0600

    working , not finished

--- adbms-web-app/controller/auth.js
+++ adbms-web-app/controller/auth.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var oracledb = require.main.require('./oracledb');
 
 router.post('/login' , function(req , res){
 	
 	var email = req.body.email;
 	var password = req.body.password;
 	console.log(email);
+
+	var obj = {
+		email: email,
+		password: password
+	}
+
+	var status = oracledb.execute(obj);
+
 	res.send('here');
 
 
