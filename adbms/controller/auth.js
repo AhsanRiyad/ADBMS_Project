866ed33d73a3d4d64174738ad3e0311a09cbf6cd
AhsanRiyad <riyad298@gmail.com>
2019-03-12 09:55:18 +0600

    go connect to oracle

--- adbms/controller/auth.js
+++ adbms/controller/auth.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/login' , function(req , res){
+	
+	var email = req.body.email;
+	var password = req.body.password;
+	console.log(email);
+	res.send('here');
 
 
+});
 
 
 module.exports = router;
